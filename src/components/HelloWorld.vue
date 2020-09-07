2ca1a2210b91912d2d8fd4827e959677aca34a2b
luotianchun <luotianchun@corp.netease.com>
2020-09-07 20:45:01 +0800

    简单使用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>hello</div>
-  <div>world</div>
+  <div>hello {{num}}</div>
+  <button @click="add">add</button>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const num = ref(0);
+    function add() {
+      num.value += 1;
+    }
+    return { num, add };
+  },
+};
 </script>
 
 <style scoped>
