27a00ef1fe9ad818e6c99d15b31d4572fc8f0815
luotianchun <luotianchun@corp.netease.com>
2020-09-14 19:02:54 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld />
-  </div>
+  <h1>todolist</h1>
+  <input type="text" v-model="newTodo" @keyup.enter="addTodo" />
+  <ul>
+    <li
+      :class="{done:item.complete}"
+      @click="toggle(index)"
+      v-for="(item,index) in todos"
+      :key="item.id"
+    >{{item.title}}</li>
+  </ul>
+  <div>{{remaining}}</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref, reactive, toRefs, computed } from "vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  setup() {
+    const state = reactive({
+      newTodo: "",
+      todos: [
+        {
+          id: 1,
+          title: "吃饭",
+          complete: true,
+        },
+        {
+          id: 2,
+          title: "喝水",
+          complete: false,
+        },
+        {
+          id: 3,
+          title: "睡觉",
+          complete: false,
+        },
+      ],
+    });
+    function addTodo() {
+      state.todos.push({
+        id: Math.random(),
+        title: state.newTodo,
+        complete: false,
+      });
+      state.newTodo = "";
+    }
+
+    function toggle(i) {
+      state.todos[i].complete = !state.todos[i].complete;
+    }
+
+    const remaining = computed(
+      () => state.todos.filter((todo) => !todo.complete).length
+    );
+    // return { state, addTodo };
+    return { ...toRefs(state), addTodo, remaining, toggle };
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.done {
+  text-decoration: line-through;
 }
 </style>
